c17422609ca793b09e4c9c5dbaad935830a37509
Catlakk <72944448+Catlakk@users.noreply.github.com>
2021-11-22 13:09:23 +0100

    Add files via upload

---
+++ image gallery/script.js
@@ -0,0 +1,7 @@
+var fullimgbox=document.getElementById("fullimgbox");
+var punaslika=document.getElementById("punaslika");
+
+function openfullimg(pic) {
+    fullimgbox.style.display="flex";
+    punaslika.src=pic;
+}
